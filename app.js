ee9e80128f774f54e6dd92b7c765d91a83f86860
Logan Martin <lsmartin914@gmail.com>
2022-06-22 00:58:33 -0400

    added the game function that simulates a game of 5 rounds

--- app.js
+++ app.js
@@ -18,14 +18,55 @@ function playRound(playerMove, computerPlay) {
     let computerMove = computerPlay();
     if (playerMove === "rock" && computerMove === "scissors") {
         console.log(`You Win! ${playerMove} beats ${computerMove}`);
+        return true;
     }
     else if (playerMove === "paper" && computerMove === "rock") {
         console.log(`You Win! ${playerMove} beats ${computerMove}`);
+        return true;
     }
     else if (playerMove === "scissors" && computerMove === "paper") {
         console.log(`You Win! ${playerMove} beats ${computerMove}`);
+        return true;
     }
     else if (playerMove === computerMove) {
         console.log("Draw!");
-    } else {console.log(`You Lose! ${computerMove} beats ${playerMove}`)}
+        return null;
+    } else {
+        console.log(`You Lose! ${computerMove} beats ${playerMove}`);
+        return false;
+    }
 }
+
+// Simulates a game of 5 rounds with the option to play again or quit
+function game() {
+    let userInput = prompt("Would you like to play? (YES or NO)");
+
+    while (userInput.toUpperCase() !== "YES"){
+        if (userInput.toUpperCase() === "NO") {
+            return console.log("Game has ended");
+        }
+        userInput = prompt("Please enter YES if you would like to play or NO if you would like to quit.");
+    }
+    console.log("Game started");
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++ ) {
+        let playerMove = prompt("Enter your move");
+        let scoreCheck = playRound(playerMove, computerPlay);
+        if (scoreCheck === true) {
+            playerScore++;
+        } 
+        else if (scoreCheck === null) {
+            playerScore++;
+            computerScore++;
+        } else {computerScore++; }
+    }
+
+    if (playerScore > computerScore) {
+        console.log("You Win This Round!");
+    } else {console.log("You Lose This Round"); }
+}
+
+game();
